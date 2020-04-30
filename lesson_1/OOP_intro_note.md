83ba51076f0808333b6451fd33eebc946f3d4197
Yu Xia <YuXia@Yu-Xias-MBP.attlocal.net>
2020-04-29 22:58:45 -0500

    Add lesson 1

---
+++ lesson_1/OOP_intro_note.md
@@ -0,0 +1,70 @@
+- OOP: a style of programming that uses objects to organize a program (a programming paradigm)
+
+  - Advantages
+    - It lets programmers think about a problem at a higher-level of abstraction, which helps them break down and solve the problem
+    - OOP helps programmer write programs that reduce the dependencies in a program, which makes maintenance easier
+    - Done right, OOP makes code flexible, easy to understand, and easy to change
+
+  - Disadvantages:
+    - OOP programs are often much larger than the equivalent procedural program
+    - OOP may lead to less efficient code; OOP programs may require more memories, disk space and computer power.
+
+- Encapsulation: the idea of bundling or combining data and operations associated with that data in a single entity; that is, it's the grouping of related properties and methods in a single object
+
+- How does encapsulation differ from encapsulation in most other OO languages?
+  - In other languages, encapsulation concerns hiding details of an object from code that uses the obejct. An object should *only* expose the methods and properties that other objects need to use the encapsulated object.
+  - However, JS doen't directly provide the means to limit exposure of methods and properties. There are ways to achieve a degree of access restriction, but they're not perfect.
+
+- When object properties have function values, we call them **methods**. 
+- `this`
+  - Suppose you change the variable name, calling a method with the original variable name will throw a reference error.
+  - when you use `this` inside a method, it refers to the object that contains the method
+
+- Objects that are used to store state within another object are called **collaborator objects** or, **collaborators**. They can be objects, primitives like strings, numbers
+
+  ```javascript
+  // here cat is collaborator
+  let cat = {
+    name: 'Fluffy',
+  
+    makeNoise() {
+      console.log('Meow! Meow!');
+    },
+  
+    eat() {
+      // implementation
+    },
+  };
+  
+  let pete = {
+    name: 'Pete',
+    pet: cat,
+  
+    printName() {
+      console.log(`My name is ${this.name}!`);
+    },
+  };
+  ```
+
+  `pete` object has a collaborator object `cat` stored in its `pet` property.
+
+  When we need to access that `pet`, we can use `pete.pet` to reference the desired state or behavior: use `pete.pet` to call the `makeNoise` method, etc...
+
+- Functions as object factories
+
+  - you need to create hundreds or thousands of similar objects
+  - One way to automate object creation is to use **object factories**: functions that create and return objects of a particular type
+
+  ```javascript
+  function createCar(make, fuelLevel, engineOn) {
+    // To be implemented by you.
+  }
+  
+  let raceCar1 = createCar('BMW', 0.5, false);
+  raceCar1.drive();
+  
+  let raceCar2 = createCar('Ferrari', 0.7, true);
+  raceCar2.drive();
+  ```
+
+  `createCar` function handles the similarities, while each invocation specifies the differences with arguments.
---
+++ lesson_1/practice_problem/object_factories.js
@@ -0,0 +1,139 @@
+// 1
+let book1 = {
+  Title: 'Mythos',
+  Author: 'Stephen Fry',
+
+  behavior() {
+    console.log(`${this.Title} was written by ${this.Author}.`);
+  }
+}
+
+let book2 = {
+  Title: 'Me Talk Pretty One Day',
+  Author: 'David Sedaris',
+
+  behavior() {
+    console.log(`${this.Title} was written by ${this.Author}.`);
+  }
+}
+
+let book3 = {
+  Title: "Aunts aren't Gentlemen",
+  Author: 'PG Wodehouse',
+
+  behavior() {
+    console.log(`${this.Title} was written by ${this.Author}.`);
+  }
+}
+
+book1.behavior();
+book2.behavior();
+book3.behavior();
+
+// 3 implement a factory function
+function createBook(title, author) {
+  return {
+    title: title,
+    author: author,
+    getDescription() {
+      return `${this.title} was written by ${this.author}.`;
+    }.
+  };
+}
+
+let book1 = createBook('Mythos', 'Stephen Fry');
+let book2 = createBook('Me Talk Pretty One Day', 'David Sedaris');
+let book3 = createBook("Aunts aren't Gentlemen", 'PG Wodehouse');
+
+book1.getDescription();  // "Mythos was written by Stephen Fry."
+book2.getDescription();  // "Me Talk Pretty One Day was written by David Sedaris."
+book3.getDescription();  // "Aunts aren't Gentlemen was written by PG Wodehouse"
+
+// use shorthand notation when a property and a variable have the same name:
+function createBook(title, author) {
+  return {
+    title,    // same as `title: title,`
+    author,
+    getDescription() {
+      return `${this.title} was written by ${this.author}.`;
+    },
+  };
+}
+
+// 4 Update the factory function so that it returns a book object that includes a property read that has an initial value of false.
+function createBook(title, author, read) {
+  return {
+    title,    
+    author,
+    read: false,
+
+    getDescription() {
+      return `${this.title} was written by ${this.author}.`;
+    },
+  };
+}
+
+// 5 Modify the factory function to use an optional read parameter with a default value of false.
+function createBook(title, author, read = false) {
+  return {
+    title,
+    author,
+    read,
+
+    getDescription() {
+      return `${this.title} was written by ${this.author}.`;
+    },
+  };
+}
+
+let book1 = createBook('Mythos', 'Stephen Fry');
+let book2 = createBook('Me Talk Pretty One Day', 'David Sedaris', false);
+let book3 = createBook("Aunts aren't Gentlemen", 'PG Wodehouse', true);
+
+console.log(book1.read); // => false
+console.log(book2.read); // => false
+console.log(book3.read); // => true
+
+// 6 Let's add a method, readBook, that marks a book object as read by setting the read property to true:
+function createBook(title, author, read = false) {
+  return {
+    title,
+    author,
+    read,
+
+    getDescription() {
+      return `${this.title} was written by ${this.author}.`;
+    },
+
+    readBook() {
+      this.read = true;
+    },
+  };
+}
+
+let book1 = createBook('Mythos', 'Stephen Fry');
+book1.readBook();
+console.log(book1.read); // => true
+
+// 7 Finally, let's update getDescription function to reflect the read state directly, For instance:
+function createBook(title, author, read = false) {
+  return {
+    title,
+    author,
+    read,
+
+    readBook() {
+      this.read = true;
+    },
+    
+    getDescription() {
+      return `${this.title} was written by ${this.author}. ` + `I ${this.read ? 'have': "haven't"} read it.`
+    },
+  };
+}
+
+let book1 = createBook('Mythos', 'Stephen Fry');
+console.log(book1.getDescription()); // Mythos was written by David Fry. I haven't read it.
+book1.readBook();
+console.log(book1.getDescription()); // Mythos was written by David Fry. I have read it.
+
